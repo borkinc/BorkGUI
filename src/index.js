9a4a5c4ebd5452e68be10b858d756086a0ee2040
javierbustillo <javier.bustillo@upr.edu>
2019-04-15 22:18:29 -0400

    Removed snack

--- src/index.js
+++ src/index.js
@@ -8,14 +8,11 @@ import {Provider} from "react-redux";
 import store from "./js/store";
 import {Router} from "react-router";
 import history from "./js/history"
-import { SnackbarProvider } from 'notistack';
 
 ReactDOM.render(
     <Provider store={store}>
         <Router history={history}>
-            <SnackbarProvider maxSnack={3}>
                 <App/>
-            </SnackbarProvider>
         </Router>
     </Provider>
     , document.getElementById('root'));
